9f7be1f545f29498e915bc096e329a0c0a9a5ce3
Marcos de Oliveira Brandão Junior <marcosde.o.b.junior@gmail.com>
2026-03-07 16:20:25 -0300

    todas as páginas

---
+++ js/mangas.js
@@ -0,0 +1,12 @@
+﻿(function () {
+  "use strict";
+
+  var pageId = document.body.getAttribute("data-page") || "";
+  var state = window.SoterStorage ? window.SoterStorage.getState() : null;
+
+  if (state && state.data) {
+    state.data.lastVisitedPage = pageId;
+    state.data.lastVisitedAt = new Date().toISOString();
+    window.SoterStorage.save(state);
+  }
+}());
